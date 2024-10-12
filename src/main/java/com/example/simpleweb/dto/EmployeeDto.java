4e1af09c33b364a595ee40833220ff194b868a3d
HCY <chienyu749@gmail.com>
2024-10-12 19:05:21 +0800

    fix: 調整 Controller 和 Service 中的異常處理 1. 新增 EmployeeDto 類別，定義員工資料傳輸字段。 2. 新增setIdZero 方法到 EmployeeService，將employee物件 id 設定為0，強制資料庫生成新的 id，而不會意外更新現有資料。 3. 修正 EmployeePageController 的 EmployeeService 依賴注入錯誤 4. 重構 addListEmployees 和 addEmployees 方法，將 EmployeeDto 轉換為 Employee 並重設 id 為 0，以確保新增操作不更新現有數據。 5. 新增 throw Exception 在 getEmployee、updateEmployee 和 deleteEmployee 方法中，以統一異常處理。

---
+++ src/main/java/com/example/simpleweb/dto/EmployeeDto.java
@@ -0,0 +1,11 @@
+package com.example.simpleweb.dto;
+
+import lombok.Data;
+
+@Data
+public class EmployeeDto {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String role;
+}
