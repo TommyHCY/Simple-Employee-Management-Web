397bf3896805647dc163b8bb05baa8beb8a4df86
HCY <chienyu749@gmail.com>
2024-10-17 00:11:49 +0800

    fix: 增加數據驗證 (Data Validation) 到 Employee API 1.新增數據驗證Annotation到EmployeeDTO 2.新增Validation依賴項到pom.xml 3.新增GlobeExceptionHandler.class處理數據驗證失敗的異常 4.重構findAll, getEmployee, addEmployees, addListEmployees, deleteEmployee方法，成功會return http狀態碼200，失敗會return http狀態碼404 5.重構updateEmployee方法，會傳入EmployeeDto參數，id將由url傳入

---
+++ src/main/java/com/example/simpleweb/exceptionhandler/GlobeExceptionHandler.java
@@ -0,0 +1,45 @@
+package com.example.simpleweb.exceptionhandler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobeExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<Map<String, List<String>>> handleValidationException(
+            MethodArgumentNotValidException validException) {
+        List<String> errorMessages = validException.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(fieldError -> fieldError.getField() + ": " + fieldError.getDefaultMessage())
+                .toList();
+
+        Map<String, List<String>> errorResponse = new HashMap<>();
+        errorResponse.put("errors", errorMessages);
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(errorResponse);
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<Map<String, String>> handleValidationException(
+            Exception e) {
+
+        Map<String, String> errorResponse = new HashMap<>();
+        errorResponse.put("error", e.getMessage());
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(errorResponse);
+    }
+}
